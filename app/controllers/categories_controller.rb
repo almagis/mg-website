0860c88b981bd49ce6cdd19ac11755c07149dcc6
Alex Gillespie <alexmgillespie@gmail.com>
2017-02-09 18:14:59 +0000

    Finished category editing fuctionality, allow index images to default to category images

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,33 +2,50 @@ class CategoriesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_category, only: [:show, :edit, :update, :destroy]
   
-  
-  
-  
   def index
-    @categories = Category.all.order("name DESC")
+    @categories = Category.all.order("name ASC")
   end
 
   def show
   end
 
   def new
+    @category = Category.new
   end
 
   def edit
   end
 
   def create
+    @category = Category.new(category_params)
+    
+    if @category.save
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+      
   end
 
   def update
+    if @category.update(category_params)
+      redirect_to categories_path
+    else
+      render 'edit'
+    end
   end
 
-  def delete
+  def destroy
+    @category.destroy
+    redirect_to categories_path
   end
   
   private
     def set_category
       @category = Category.find(params[:id])
+    end
+    
+    def category_params
+      params.require(:category).permit(:name, :category_img)
     end
 end
