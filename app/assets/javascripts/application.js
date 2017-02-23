f53b8d95575a6e4fec886e3aadcc66f03bdaccd5
Alex Gillespie <alexmgillespie@gmail.com>
2017-02-23 16:58:09 +0000

    Added page titles to show up in tabs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,26 +18,29 @@
 //= require_tree .
 //= require tinymce-jquery
 
+// Initialiser for tiny MCE rich text editor
 tinymce.init({
   selector: '.textarea',
   content_css: "/app/assets/stylesheets/application.css.scss"
 });
 
 $(document).ready(function(){
+  // Overriding rich text styling for article content
   $('.article-body>p').addClass('article-text');
   $('.article-body>p').css({'color': '#333333','font-family': 'Lora','font-size': '20px','line-width': '28px'});
   $('.article-body>p>a').css({'color': '#4286f4'}).attr("target","_blank");
   
-  if ($('.article-date').length) {
-    $('.article-date').each( function(index) {
+  // This code block formats the date so that the ordinal indicator (i.e. 1st, 2nd) is correct
+  if ($('.article-date').length) { // Checks to see if there is a date element on the page
+    $('.article-date').each( function(index) { // Cycles through each of the date elements on the page
       if ($.inArray($(':nth-child(1)', this).html(), ['1','21','31']) > -1) {
-        $(':nth-child(2)', this).html('st');
+        $(':nth-child(2)', this).html('st'); // Matches the value of the day of the month (first child) to the correct indicator (second child)
       } else if ($.inArray($(':nth-child(1)', this).html(), ['2','22']) > -1) {
         $(':nth-child(2)', this).html('nd');
       } else if ($.inArray($(':nth-child(1)', this).html(), ['3','23']) > -1) {
         $(':nth-child(2)', this).html('rd');
       } else {
-        $(':nth-child(2)', this).html('th');
+        $(':nth-child(2)', this).html('th'); // Defaults to 'th'
       }
     });
   }
